97f8c9181fb1079c8d77cfe94cfc681ec66b68e1
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-03-16 20:26:34 -0300

    initial commit

---
+++ src/main/java/br/com/deveficiente/bolaoapi/BolaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.bolaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/deveficiente/bolaoapi/BolaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.bolaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
